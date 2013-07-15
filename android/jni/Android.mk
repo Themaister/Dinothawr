41704247c58b3b48b706adad67a914093ed48e5d
Themaister <maister@archlinux.us>
2013-07-15 20:32:51 +0200

    Fixup Android build.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -5,20 +5,22 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := retro_dino
 
 ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
+  LOCAL_CFLAGS += -DANDROID_ARM
+  LOCAL_ARM_MODE := arm
 endif
 
 ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS += -DANDROID_X86
+  LOCAL_CFLAGS += -DANDROID_X86
 endif
 
 ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS
+  LOCAL_CFLAGS += -DANDROID_MIPS
 endif
 
-LOCAL_SRC_FILES += $(wildcard ../../*.cpp) $(wildcard ../../*/*.cpp)
-LOCAL_CXXFLAGS += -O2 -Wall -std=gnu++0x -ffast-math -fexceptions
+LOCAL_SRC_FILES += $(wildcard ../../*.cpp) $(wildcard ../../*/*.cpp) $(wildcard ../../vorbis/*.c) $(wildcard ../../ogg/*.c)
+LOCAL_CPPFLAGS += -Wall -std=gnu++11 -fexceptions -DOV_EXCLUDE_STATIC_CALLBACKS 
+LOCAL_CFLAGS += -O2 -ffast-math -D_GLIBCXX_HAS_GTHREADS
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../.. $(LOCAL_PATH)/../../vorbis
 LOCAL_LDLIBS += -lz
 
 include $(BUILD_SHARED_LIBRARY)
